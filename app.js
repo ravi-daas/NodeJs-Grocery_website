776d6959e219f18fb4850fda9b0e62b6b6d946d3
Ravi <ravirao1105@gmail.com>
2023-10-25 23:10:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-// require('dotenv').config({ path: '../.env' });
-
 const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
@@ -15,20 +13,10 @@ app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
 
-// const authenticate = require('./routes/authenticate');
-
 const loadHome = require('./routes/home');
-
-// const product = require('./routes/product');
-
-// const user = require('./routes/user');
-
 const cart = require('./routes/cart');
 
-// app.use(authenticate);
 app.use(loadHome);
-// app.use(product);
-// app.use(user);
 app.use(cart);
 
 app.use((req, res) => {
